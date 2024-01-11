56cd9256e635f03880b0543bed2001481cbb61cc
saishanker27 <shankeremmadi27@gmail.com>
2024-01-10 23:12:27 -0800

    student program created

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public static void main(String args[]) {
+        Student s = new Student();
+        String name;
+        name=s.getName();
+       System.out.println(name);
+    }
+    public String getName(){
+        String name="Shanker";
+        return name;
+    }
+
+}
