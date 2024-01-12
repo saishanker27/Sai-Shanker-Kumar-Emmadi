43eb2c7d7d3a8132595794258638dd8edbbdafc1
saishanker27 <shankeremmadi27@gmail.com>
2024-01-12 01:07:03 -0800

    Store class with Department created

---
+++ Store.java
@@ -0,0 +1,16 @@
+abstract class Department {
+    int size=69;
+    public abstract int getDepartmentsize();
+}
+class Departmentsize extends Department{
+    @Override
+    public int getDepartmentsize() {
+        return size;
+    }
+}
+public class Store{
+    public static void main(String args[]){
+        Departmentsize d = new Departmentsize();
+        System.out.println(d.getDepartmentsize());
+    }
+}
