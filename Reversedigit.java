03ad8988f832483a109860b9c69c31307d8f41cf
saishanker27 <shankeremmadi27@gmail.com>
2024-01-17 11:01:01 -0800

    Reverse program is added

---
+++ Reversedigit.java
@@ -0,0 +1,15 @@
+public class Reversedigit {
+    public static void main(String args[]) {
+
+        int a;
+        int x = 12345;
+        while(x>0){
+
+            a = x % 10;
+            System.out.print(a);
+            x = x / 10;
+
+
+        }
+    }
+}
