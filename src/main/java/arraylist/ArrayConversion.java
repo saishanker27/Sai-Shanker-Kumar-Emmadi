b59eb401e19a0f3a2cdd2b7f8ba1fd06afbeb7a3
saishanker27 <shankeremmadi27@gmail.com>
2024-01-25 15:41:08 -0800

    added

--- src/main/java/arraylist/ArrayConversion.java
+++ src/main/java/arraylist/ArrayConversion.java
@@ -4,17 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayConversion {
-    public static void main(String[] args) {
+    public static String[] arrayConversion(ArrayList<Student> studentList) {
         // Create an ArrayList
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Texas");
-        arrayList.add("California");
-        arrayList.add("Chicago");
 
         // Convert ArrayList to array
         String[] array = arrayList.toArray(new String[0]);
+        return array;
 
-        // Print the array
-        System.out.println("Array elements: " + Arrays.toString(array));
     }
 }
