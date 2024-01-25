0c017b276e009e1f52fba143730903ae2d1c5361
saishanker27 <shankeremmadi27@gmail.com>
2024-01-25 15:40:18 -0800

    added

--- src/main/java/arraylist/RemoveStudentsBelowAverage.java
+++ src/main/java/arraylist/RemoveStudentsBelowAverage.java
@@ -14,24 +14,6 @@ public class RemoveStudentsBelowAverage {
         // Remove students with GPA below average
         studentList.removeIf(student -> student.gpa < averageGPA);
     }
-    public static void main(String[] args) {
-        // Example usage
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student("John", "Crew", 3.4));
-        students.add(new Student("Jenny", "Smith", 3.2));
-        students.add(new Student("Jessie", "Johnson", 3.7));
-
-        arraylist.RemoveStudentsBelowAverage remover = new arraylist.RemoveStudentsBelowAverage();
-
-        System.out.println("Before removal:");
-        printStudents(students);
-
-        remover.removeBelowAverageGPA(students);
-
-        System.out.println("\nAfter removal:");
-        printStudents(students);
-    }
-
     private static void printStudents(ArrayList<Student> students) {
         for (Student student : students) {
             System.out.println("Name: " + student.firstName + " " + student.lastName + ", GPA: " + student.gpa);
